614926c189dc17007edc14a889dc8fe527039b59
Damien Barchowsky <dbarchowsky@gmail.com>
2016-02-12 21:09:03 -0800

    Add landing page stylesheet #5

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,27 @@
 var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
 
+var sassSrc = "scss/{app,landing}.scss";
+
 var sassPaths = [
-  'bower_components/foundation-sites/scss',
-  'bower_components/motion-ui/src'
+    'bower_components/foundation-sites/scss',
+    'bower_components/motion-ui/src'
 ];
 
 gulp.task('sass', function() {
-  return gulp.src('scss/app.scss')
-    .pipe($.sass({
-      includePaths: sassPaths
-    })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
-    .pipe(gulp.dest('static/css'));
+    return gulp.src(sassSrc)
+        .pipe($.sass({
+            includePaths: sassPaths
+        })
+        .on('error', $.sass.logError))
+        .pipe($.autoprefixer({
+            browsers: ['last 2 versions', 'ie >= 9']
+        }))
+        .pipe(gulp.dest('static/css'));
 });
 
-gulp.task('default', ['sass'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
+gulp.task('watch', function() {
+    gulp.watch(['scss/**/*.scss'], ['sass']);
 });
+
+gulp.task('default', ['sass', 'watch']);
